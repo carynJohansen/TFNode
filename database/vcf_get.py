e1e54404fa956f5cf3e37fe89c3bc125461fcc76
mirrormere <ckjohan@gmail.com>
2015-09-01 12:12:24 -0400

    adding vcf_get.py

---
+++ database/vcf_get.py
@@ -0,0 +1,25 @@
+###############################
+#           Env               #
+
+from BCBio import GFF
+from sqlalchemy import create_engine
+from pandas.io import sql
+import numpy as np
+import pandas as pd
+
+import config
+import vcf
+
+#to time the program:
+import time
+
+###############################
+#         SQL Engine          #
+
+engine = create_engine('sqlite:///' + config.DATABASE)
+connect = engine.connect().connection
+#print engine
+
+###############################
+#          Methods            #
+
