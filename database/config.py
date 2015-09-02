5c79bd6bb28eea98184b5bd8fc7853da76edb6a4
mirrormere <ckjohan@gmail.com>
2015-09-02 13:27:23 -0400

    working on vcf issue

--- database/config.py
+++ database/config.py
@@ -1,10 +1,11 @@
 #Variables
 
-DATABASE = '/scratch/ckj239/michael.db'
-GFF = '../../data/all.gff3'
+DATABASE = '//Users/carynjohansen/Documents/NYUClasses/Purugganan_Lab/TFInteraction_db/michael.db'
+GFF = '/Users/carynjohansen/Documents/NYUClasses/Purugganan_Lab/TFInteraction_db/data/all.gff3'
 
-NET = '/home/ckj239/TF_Interaction/data/summary_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.tsv'
+NET = '/Users/carynjohansen/Documents/NYUClasses/Purugganan_Lab/TFInteraction_db/data/summary_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.tsv'
 
 CHROMOSOMES = ["Chr1", "Chr2", "Chr3", "Chr4", "Chr5", "Chr6", "Chr7", "Chr8","Chr9", "Chr10", "Chr11", "Chr12"]
 
-VCF = '/home/ckj239/TF_Interaction/data/rice_chr2_3.vcf'
+#VCF = '/Users/carynjohansen/Documents/NYUClasses/Purugganan_Lab/TFInteraction_db/data/rice_chr2_3.vcf'
+VCF_COMP = '/Users/carynjohansen/Documents/NYUClasses/Purugganan_Lab/TFInteraction_db/data/rice_chr2_3.vcf.gz'
